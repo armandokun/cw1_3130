8c078119a51015578719923546048abb8f78b390
armandokun <armandasvaicikauskas@gmail.com>
2019-12-09 17:55:45 +0000

    namings updated

--- src/main/java/cst3130/armandokun/App.java
+++ src/main/java/cst3130/armandokun/App.java
@@ -1,17 +1,11 @@
 package cst3130.armandokun;
 
-import org.springframework.context.*;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import cst3130.armandokun.iPhoneScraper;
+import cst3130.armandokun.webscraping.*;
 
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Beginning Scrapping...");
-        new iPhoneScraper();
+        new argosScraper();
     }
-
-    /** Uses Spring Annotation configuration to set up and run application */
-
 }
--- src/main/java/cst3130/armandokun/webscraping/argosScraper.java
+++ src/main/java/cst3130/armandokun/webscraping/argosScraper.java
@@ -1,4 +1,4 @@
-package cst3130.armandokun;
+package cst3130.armandokun.webscraping;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -6,13 +6,13 @@ import org.jsoup.select.Elements;
 
 
 /** Example code to illustrate web scraping with JSoup */
-public class iPhoneScraper {
+public class argosScraper {
     
     
     /** Constructor */
-    iPhoneScraper(){
+    public argosScraper(){
         try{
-            scrapeIphones();
+            scrapeArgos();
         }
         catch(Exception ex){
             ex.printStackTrace();
@@ -21,36 +21,36 @@ public class iPhoneScraper {
     
     
     /** Scrapes cornflakes data from the Ocado website */
-    void scrapeIphones() throws Exception{
+    void scrapeArgos() throws Exception{
         //Name of item that we want to scrape
-        String itemName = "phones";
+        String itemName = "iphones";
         
         //Download HTML document from website
         Document doc = Jsoup.connect("https://www.argos.co.uk/search/" + itemName).get();
         
         //Get all of the products on the page
-        Elements prodsLayer1 = doc.select(".ProductCardstyles__Wrapper-l8f8q8-0");
-        Elements prodsLayer2 = prodsLayer1.select(".dlOkBP");
+        Elements productsLayer = doc.select(".ProductCardstyles__Wrapper-l8f8q8-0");
+        Elements products = productsLayer.select(".dlOkBP");
         
         //Work through the products
-        for(int i=0; i<prodsLayer2.size(); ++i){
+        for(int i=0; i<products.size(); ++i){
             
             //Get the product description
-            Elements description = prodsLayer2.get(i).select(".ProductCardstyles__Title-l8f8q8-11.kLyOND");
+            Elements description = products.get(i).select(".ProductCardstyles__Title-l8f8q8-11.kLyOND");
             
             //Get the product price
-            Elements price1 = prodsLayer2.get(i).select(".fHBARv");
+            Elements price1 = products.get(i).select(".fHBARv");
 
             //Deletes pound symbol from the price and formats to float
             String scrapedPrice = price1.text().replace("£", ""); 
             Float price = Float.parseFloat(scrapedPrice);
 
             //Get the image url
-            Elements image = prodsLayer2.get(i).select(".dnXqZD");
+            Elements image = products.get(i).select(".dnXqZD");
             String imageUrl = image.select("img").attr("src");
 
             //Get product url
-            Elements productLink = prodsLayer2.get(i).select(".btn-cta");
+            Elements productLink = products.get(i).select(".btn-cta");
             String productUrl = productLink.attr("href");
             
             //Output the data that we have downloaded
