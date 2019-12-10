2c521ada3d04f8f75af1bc3a5eb37d85cf686726
armandokun <armandasvaicikauskas@gmail.com>
2019-12-10 13:38:35 +0000

    simplified main method

---
+++ src/main/java/cst3130/armandokun/webscraping/ScraperManager.java
@@ -0,0 +1,48 @@
+package cst3130.armandokun.webscraping;
+
+import java.util.Scanner;
+
+public class ScraperManager {
+
+    // Default Constructor
+    public ScraperManager() {
+
+    }
+
+    public void startScrapers() {
+
+        // Create the scraper classes
+        argosScraper scraper1 = new argosScraper();
+        onbuyScraper scraper2 = new onbuyScraper();
+        freemansScraper scraper3 = new freemansScraper();
+
+        // Start the threads running.
+        scraper1.start();
+        scraper2.start();
+        scraper3.start();
+
+        // Read input from user until they type 'stop'
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("stop")) {
+            userInput = scanner.nextLine();
+        }
+
+        // Stop threads
+        scraper1.stopThread();
+        scraper2.stopThread();
+        scraper3.stopThread();
+
+        // Wait for threads to finish - join can throw an InterruptedException
+        try {
+            scraper1.join();
+            scraper2.join();
+            scraper3.join();
+        } catch (InterruptedException ex) {
+            System.out.println("Interrupted exception thrown: " + ex.getMessage());
+        }
+
+        System.out.println("Web scraping complete");
+
+    }
+}
